120158322fe2e597815064edd016dad2d0bd1c94
Artureczek <arturkielbiewski@gmail.com>
2018-10-07 21:23:55 +0200

    Poprawka camel case 2

--- src/main/java/com/solodive/championselect/service/UserService.java
+++ src/main/java/com/solodive/championselect/service/UserService.java
@@ -78,7 +78,7 @@ public class UserService {
 
     public Optional<User> requestPasswordReset(String mail) {
 
-        String somestring = "somestring";
+        String someString = "someString";
 
         return userRepository.findOneByEmailIgnoreCase(mail)
             .filter(User::getActivated)
